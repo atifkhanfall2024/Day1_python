068c553cb83bcc7968c4bf6cb5a9ec9b86f17a62
MUhammad Atif khan <muhammadatifkhan0906@gmail.com>
2024-10-03 19:30:00 +0500

    first commit

---
+++ Day1/variables.py
@@ -0,0 +1,17 @@
+
+   # first of all we should know about it that what is variable
+  # its an object for storing specific values
+
+x= 10
+print(x)
+
+xx = "how are you boss ?"
+print(xx)
+
+x = x+20
+print(x)
+
+
+# to check type of a variable type(xx)
+
+print(type(xx))
